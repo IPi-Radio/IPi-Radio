9fde59c13525f7c2dd41a4e35f130540ada36acc
zocker-160 <zocker1600@posteo.net>
2023-10-30 18:58:48 +0100

    version 2.1

--- src/constants.py
+++ src/constants.py
@@ -5,8 +5,8 @@ import os
 curr_location = os.path.dirname(os.path.realpath(__file__))
 getAbsPath = lambda x, y: os.path.join(curr_location, x, y)
 
-VERSION = "2.0"
-VERSION_URL = "https://raw.githubusercontent.com/IPi-Radio/IPi-Radio/v2/VERSION"
+VERSION = "2.1"
+VERSION_URL = "https://raw.githubusercontent.com/IPi-Radio/IPi-Radio/master/VERSION"
 
 STATIONS = getAbsPath("settings", "stations.json")
 SETTINGS = getAbsPath("settings", "settings.json")
